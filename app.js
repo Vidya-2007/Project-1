5ade4fabd3e129df89f1f09b32f52503a838665a
Vidya-2007 <vidyavmanjule24@coep.sveri.ac.in>
2026-01-09 14:46:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+let userScore=0;
+let compScore=0;
+
+const choices=document.querySelectorAll(".choice");
+const msg=document.querySelector("#msg");
+
+const userScorePara=document.querySelector("#user-score");
+const compScorePara=document.querySelector("#comp-score");
+
+const genCompChoice =() =>{
+    const options=["rock","paper","scissor"];
+    const randIdx=Math.floor(Math.random()*3);
+    return options[randIdx];
+};
+const drawGame=() =>{
+    
+    msg.innerText="Game was draw.Play again.";
+    msg.style.backgroundColor="black";
+}  
+const showWinner=(userWin,userChoice,compChoice)=>{
+    if(userWin) {
+        
+        userScore++;
+        userScorePara.innerText=userScore;
+        msg.innerText=`You Win!Your ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor="red";
+    }else {
+    compScore++;
+    compScorePara.innerText=compScore;
+    
+    msg.innerText=`You lose.${compChoice} beats your ${userChoice}`;
+    msg.style.backgroundColor="green";
+    }
+}
+const playGame=(userChoice)=>{
+    console.log("userChoice is:",userChoice);
+    //generate computer choice
+    const compChoice=genCompChoice();
+    console.log("computer choice:",compChoice);
+
+    if(userChoice===compChoice){
+        //Draw game
+        drawGame();
+    } else {
+        let userWin=true;
+        if(userChoice==="rock"){
+            //scissors,rock
+            userWin=compChoice==="paper"?false:true;
+        }else if(userChoice==="paper"){
+            //rock,scissor
+            userWin=compChoice==="scissor"?false:true;
+        }
+        else {
+            //rock,paper
+            userWin=compChoice==="rock"?false:true;
+        }
+        showWinner(userWin,userChoice,compChoice);
+    }
+};
+choices.forEach((choice)=>{
+    choice.addEventListener("click",()=>{
+        const userChoice=choice.getAttribute("id");
+        playGame(userChoice);
+    })
+})
